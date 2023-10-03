36e8840b555a631fc0cb75cf67414d1fb243a797
Manasse Daniel <danidkm242@gmail.com>
2023-10-03 14:51:44 +0100

    hide api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: import.meta.env.API_URL,
   params: {
-    key: "d163ff185e9b4c8dac77d6c1a2141bb4"
+    key: import.meta.env.API_KEY
   }
 })
